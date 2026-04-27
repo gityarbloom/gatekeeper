c023bb625e76d1832e809192df45cca6b28fbdba
Israel-Yarbloom <i0504170922@gmail.com>
2026-04-27 09:20:51 +0300

    added main.py

--- end_user_service/main.py
+++ end_user_service/main.py
@@ -1,1 +1,10 @@
 from fastapi import FastAPI
+from routes import router
+import uvicorn
+
+
+app = FastAPI()
+app.include_router(router=router)
+
+if __name__ == "__main__":
+    uvicorn.run(app=app)
